1ee7370be73f9b178e4683a9e4ee6a1621f24927
JeRomeGrill <glushko.r@gmail.com>
2022-07-13 17:09:19 +0300

    Filled up Animal class

--- Animal.java
+++ Animal.java
@@ -1,2 +1,33 @@
 public class Animal {
+    private String name = "Animal test";
+    private int age = 18;
+
+
+    public Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Animal() {
+    }
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
