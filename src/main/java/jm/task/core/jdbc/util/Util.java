6fd37720d4f084366cf5530d2f84dc93edb84b8f
JeRomeGrill <glushko.r@gmail.com>
2022-07-15 12:53:37 +0300

    Connection variables added to Util class

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,15 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.Statement;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    public static final String USER_NAME = "root";
+    public static final String PASSWORD = "root";
+    public static final String URL = "jdbc:mysql://localhost:3306/mysql";
+    public static Statement statement;
+    public static Connection connection;
+
+
 }
